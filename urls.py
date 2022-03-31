6bba33435dbf763a673cc52bf01def710a2f353d
Axel2408 <axelrubenacker46@gmail.com>
2022-03-31 01:44:07 -0300

    Initial commit

---
+++ urls.py
@@ -0,0 +1,30 @@
+"""tpfinal_axel URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from tpfinal_axel.views import primervista, segundavista,diahora_tercervista,nombre_cuartavista, edad_usuario_quintavista, pag_inicio
+
+
+urlpatterns = [
+path('admin/', admin.site.urls),
+path('primero/', primervista),
+path('segundo/', segundavista),
+path('tercero/', diahora_tercervista),
+path('cuarto/<nombre>',nombre_cuartavista ), 
+path('quinto/<edad>',edad_usuario_quintavista ),
+path('inicio/', pag_inicio)
+]
+    
---
+++ views.py
@@ -0,0 +1,37 @@
+from multiprocessing import context
+from tempfile import template
+from django.http import HttpResponse
+from django.template import Template, Context, loader
+from datetime import datetime
+
+
+def primervista (request):
+    return HttpResponse("Gimnasio Squat")
+
+def segundavista(request):
+    return HttpResponse ("<h1> Gimnasio Squat </h1> <p> Hacelo bien, Hacelo Squat </p>")
+
+def diahora_tercervista(request):
+    fecha= datetime.now()
+    return HttpResponse(f"<p> el tiempo ahora es {fecha} </p>")
+
+def nombre_cuartavista(request, nombre):
+    return HttpResponse(f"bienvenido/a {nombre}")
+#ver el tema del nombre, no me aparece
+
+def edad_usuario_quintavista(request, edad):
+    anio_nac= 2022- int(edad)
+    return HttpResponse("la edad no es un limite para lograr tus objetivos, puedes hacerlo")
+#funciono pero a mipagina no le va a servir mucho la edad asi que no puse un f stream
+
+def pag_inicio(request):
+   archivo=open(r"C:\Users\PC\Desktop\PYTHON\TPfinal\tpfinal_axel\tpfinal_axel\templates\iniciopag.html", 'r')
+   plantilla= template(archivo.read())
+
+   archivo.close
+
+   contexto= context()
+
+   documento=plantilla.render(contexto)
+
+   return HttpResponse(documento)
